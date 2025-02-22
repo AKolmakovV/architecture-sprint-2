b2c45315bc967f2139d2bf0bfcbb2b6cd0ca7cf2
akolmakov <akolmakov-v@yandex.ru>
2025-02-22 09:31:07 +0300

    Задание 1: маленький рефакторинг в файлах инициализации роута и заполнения данными

--- mongo-sharding/scripts/init-router.sh
+++ mongo-sharding/scripts/init-router.sh
@@ -9,5 +9,7 @@ sh.addShard("shard1/shard1:27018");
 sh.addShard("shard2/shard2:27019");
 
 sh.enableSharding("somedb");
-sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+db.createCollection("helloDoc")
+db.helloDoc.createIndex({ "name": "hashed" });
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } );
 EOF
--- mongo-sharding/scripts/mongo-fill-data.sh
+++ mongo-sharding/scripts/mongo-fill-data.sh
@@ -7,11 +7,6 @@
 
 docker compose exec -T mongo_router mongosh --port 27020 --quiet <<EOF
 use somedb;
-sh.enableSharding("somedb");
-db.createCollection("helloDoc")
-db.helloDoc.createIndex({ "name": "hashed" });
-sh.shardCollection("somedb.helloDoc", { "name" : "hashed" });
-
 for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i});
 db.helloDoc.countDocuments();
 EOF
