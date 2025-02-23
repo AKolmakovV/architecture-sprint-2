77dc491eeec6f6abbf0c25d2578c5eed5644d535
akolmakov <akolmakov-v@yandex.ru>
2025-02-23 06:32:37 +0300

    Задание номер 3: Репликация + дополнительная детализация на схеме

--- mongo-sharding-repl/scripts/remove-all.sh
+++ mongo-sharding-repl/scripts/remove-all.sh
@@ -11,7 +11,11 @@ docker ps
 echo -e "Начало удаления контейнеров..."
 
 docker container rm -f -v shard1
+docker container rm -f -v shard1-slave1
+docker container rm -f -v shard1-slave2
 docker container rm -f -v shard2
+docker container rm -f -v shard2-slave1
+docker container rm -f -v shard2-slave2
 docker container rm -f -v configSrv
 docker container rm -f -v mongo_router
 docker container rm -f -v pymongo_api
@@ -29,9 +33,13 @@ docker volume ls
 
 echo -e "Начало удаления volume..."
 
-docker volume rm mongo-sharding_shard1-data
-docker volume rm mongo-sharding_shard2-data
-docker volume rm mongo-sharding_config-data
+docker volume rm mongo-sharding-repl_config-data
+docker volume rm mongo-sharding-repl_shard1-data
+docker volume rm mongo-sharding-repl_shard1-slave1-data
+docker volume rm mongo-sharding-repl_shard1-slave2-data
+docker volume rm mongo-sharding-repl_shard2-data
+docker volume rm mongo-sharding-repl_shard2-slave1-data
+docker volume rm mongo-sharding-repl_shard2-slave2-data
 
 echo -e "Список volume после удаления"
 docker volume ls 
@@ -44,9 +52,10 @@ docker volume ls
 echo -e "Список сетей до удаления"
 docker network ls
 
+
 echo -e "Начало удаления network..."
 
-docker network rm mongo-sharding_app-network
+docker network rm mongo-sharding-repl_app-network
 
 echo -e "Список сетей после удаления"
 docker network ls
